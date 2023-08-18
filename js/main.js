1d95fc09afdf15c3b1e7c5f3be3f5eb3358f1f48
LittleMoki <steamlittle659@gmail.com>
2023-08-18 18:56:16 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let burger = document.getElementById('burger')
+let popup = document.getElementById('popup')
+
+burger.addEventListener('click', ()=>{
+  popup.classList.toggle('active')
+})
+popup.addEventListener('mouseleave', ()=>{
+  popup.classList.remove('active')
+})
